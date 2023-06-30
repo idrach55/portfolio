9d1a1a889dec996389b7c35534b27dc3f0baa66c
Isaac Drachman <isaacdrachman@Isaacs-MacBook-Pro.local>
2023-06-29 20:35:04 -0400

    remove unused APIs

--- portfolio/services.py
+++ portfolio/services.py
@@ -9,12 +9,10 @@ from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
-import quandl
 import requests
 
 # CONSTANTS
 KEY_DIR = 'keys'
-
 
 class AlphaVantage:
     def __init__(self, api_key: Optional[str] = None):
@@ -64,32 +62,6 @@ class AlphaVantage:
         r = requests.get(url)
         return r.json()
 
-
-class Quandl:
-    def __init__(self, token: Optional[str] = None):
-        self.token = token
-        if self.token is None:
-            self.token = open(f'{KEY_DIR}/quandl.keys','r').read().split('\n')[0]
-        elif self.token[-5:] == '.keys':
-            self.token = open(token,'r').read().split('\n')[0]
-
-    def get_yield_curve(self) -> pd.DataFrame:
-        def rename_col(col):
-            if 'MO' in col:
-                return col[:col.find(' ')]+'m'
-            elif 'YR' in col:
-                return col[:col.find(' ')]+'y'
-        data = quandl.get('USTREASURY/YIELD', authtoken=self.token)
-        data.columns = [rename_col(col) for col in data.columns]
-        return data
-
-    def get_ndx_factor_idx(self, factors: List[str]) -> pd.DataFrame:
-        data = pd.DataFrame()
-        for factor in factors:
-            data[factor] = quandl.get('NASDAQOMX/{}'.format(factor), authtoken=self.token)['Index Value']
-        return data
-
-
 class Fred:
     def __init__(self):
         self.url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id={}'
@@ -105,85 +77,3 @@ class Fred:
         df = pd.read_csv(self.url.format(symbol),index_col=0)
         df.index = pd.to_datetime(df.index)
         return df[symbol]
-
-
-# Unused services: Polygon, IEX
-class Polygon:
-    def __init__(self, api_key: Optional[str] = None):
-        # Process api key as either given or filename
-        self.api_key = api_key
-        if self.api_key[-5:] == '.keys':
-            self.api_key = open(api_key,'r').read().split('\n')[0]
-        self.root_url = 'https://api.polygon.io/v2/'
-
-    def aggs(self, ticker: str, from_date: str, to_date: str, timespan: str= 'day', unadjusted: bool = False) -> pd.DataFrame:
-        supported_spans = ['minute','hour','day','week','month','quarter','year']
-        assert timespan in supported_spans, f'timespan {timespan} not supported'
-
-        # Construct url for this request and GET it
-        url = self.root_url+'aggs/ticker/{}/range/1/{}/{}/{}?unadjusted={}&sort=asc&apiKey={}'
-        r = requests.get(url.format(ticker,timespan,from_date,to_date,unadjusted,self.api_key))
-
-        # Cast to pandas dataframe and clean
-        data = pd.DataFrame(r.json()['results'])
-        data.index = pd.to_datetime(data.t, unit='ms')
-        data.index = pd.to_datetime(data.index.date)
-        return data.drop('t',axis=1)
-
-    def dividends(self, ticker: str, adjusted: bool = True) -> pd.DataFrame:
-        url = self.root_url+'reference/dividends/{}?apiKey={}'
-        r = requests.get(url.format(ticker,self.api_key))
-
-        data = pd.DataFrame(r.json()['results'])
-        data.exDate = pd.to_datetime(data.exDate)
-        if not adjusted:
-            return data
-
-        factor = pd.Series([1.0]*len(data), index=data.exDate)
-        stock_splits = self.splits(ticker)
-        stock_splits = stock_splits.loc[stock_splits.exDate > factor.index[-1]]
-        for idx,split in stock_splits.iterrows():
-            factor[factor.index < split.exDate] *= split.ratio
-        data_adj = data.copy()
-        data_adj.index = data_adj.exDate
-        data_adj.amount *= factor
-        return data_adj
-
-    def splits(self, ticker: str) -> pd.DataFrame:
-        url = self.root_url+'reference/splits/{}?apiKey={}'
-        r = requests.get(url.format(ticker,self.api_key))
-        data = pd.DataFrame(r.json()['results'])
-        data.exDate = pd.to_datetime(data.exDate)
-        return data
-
-
-class IEX:
-    def __init__(self, token: Optional[str] = None):
-        self.token = token
-        if self.token[-5:] == '.keys':
-            self.token = open(token,'r').read().split('\n')[0]
-        self.root_url = 'https://cloud.iexapis.com/stable/'
-
-    def chart(self, symbol: str, range: str, close_only: bool = True) -> pd.DataFrame:
-        url = self.root_url+'stock/{}/chart/{}?chartCloseOnly={}&token={}'
-        r = requests.get(url.format(symbol,range,close_only,self.token))
-        data = pd.DataFrame(r.json())
-        data.index = pd.to_datetime(data.date)
-        data = data.drop('date',axis=1)
-        return data
-
-    # Return adjusted closes for several symbols over a range
-    def charts(self, symbols: List[str], range: str) -> pd.DataFrame:
-        data = pd.DataFrame()
-        for symbol in symbols:
-            price_data = self.chart(symbol, range, close_only=True)
-            data[symbol] = price_data.close
-        return data
-
-    def dividends(self, symbol: str, range: str) -> pd.DataFrame:
-        url = self.root_url+'stock/{}/dividends/{}?token={}'
-        r = requests.get(url.format(symbol,range,self.token))
-        data = pd.DataFrame(r.json())
-        for column in ['exDate','paymentDate','recordDate','declaredDate']:
-            data[column] = pd.to_datetime(data[column])
-        return data
